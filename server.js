6cdd9362f93a3455b68a23e92d954b4203c07123
Baraka124 <85062427+Baraka124@users.noreply.github.com>
2025-12-30 14:05:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -15,8 +15,11 @@ app.use(helmet({
         directives: {
             defaultSrc: ["'self'"],
             styleSrc: ["'self'", "'unsafe-inline'"],
-            scriptSrc: ["'self'", "'unsafe-inline'"],
-            imgSrc: ["'self'", "data:", "https:"]
+            scriptSrc: ["'self'"],
+            scriptSrcAttr: ["'self'"],
+            imgSrc: ["'self'", "data:", "https:"],
+            connectSrc: ["'self'", "ws:", "wss:"],
+            fontSrc: ["'self'", "data:"],
         }
     }
 }));
